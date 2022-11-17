873a3828b721223b66bbe5d5c9bf5500dc0b2cf0
Muammer Diri <muammerdiri2@gmail.com>
2022-11-17 20:29:21 +0300

    GetAllEmployerResponse

---
+++ src/main/java/com/muammerdiri/hrms/business/responses/GetAllEmployerResponse.java
@@ -0,0 +1,15 @@
+package com.muammerdiri.hrms.business.responses;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class GetAllEmployerResponse {
+    private int id;
+    private String companyName;
+    private String website;
+    private String phoneNumber;
+}
