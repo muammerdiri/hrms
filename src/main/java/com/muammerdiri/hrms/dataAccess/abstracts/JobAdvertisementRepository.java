8c646cb2e05264e467f33c4eeba22c942a022752
Muammer Diri <muammerdiri2@gmail.com>
2022-11-18 10:34:55 +0300

    Job Advertisement is created repository

--- src/main/java/com/muammerdiri/hrms/dataAccess/abstracts/JobAdvertisementRepository.java
+++ src/main/java/com/muammerdiri/hrms/dataAccess/abstracts/JobAdvertisementRepository.java
@@ -1,2 +1,16 @@
-package com.muammerdiri.hrms.dataAccess.abstracts;public class JobAdvertisementRepository {
+package com.muammerdiri.hrms.dataAccess.abstracts;
+
+import com.muammerdiri.hrms.core.utilities.results.DataResult;
+import com.muammerdiri.hrms.entites.concretes.JobAdvertisement;
+import net.bytebuddy.TypeCache;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface JobAdvertisementRepository extends JpaRepository<JobAdvertisement,Integer> {
+
+    List<JobAdvertisement> findAllByStatusTrue();
+
 }
