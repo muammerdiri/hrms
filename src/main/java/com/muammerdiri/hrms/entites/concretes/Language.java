b724ed9971b1e0f10a7906d2095eb9a26fc8eb95
Muammer Diri <muammerdiri2@gmail.com>
2022-11-19 18:47:51 +0300

    Language is created.

---
+++ src/main/java/com/muammerdiri/hrms/entites/concretes/Language.java
@@ -0,0 +1,26 @@
+package com.muammerdiri.hrms.entites.concretes;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name="languages")
+@Entity
+public class Language {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "level")
+    private short level;
+
+}
