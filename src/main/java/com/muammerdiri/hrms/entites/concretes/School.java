a1466a57c81414b4a84aaa1400b69fb49ab55607
Muammer Diri <muammerdiri2@gmail.com>
2022-11-19 19:17:19 +0300

    School object is created.

---
+++ src/main/java/com/muammerdiri/hrms/entites/concretes/School.java
@@ -0,0 +1,43 @@
+package com.muammerdiri.hrms.entites.concretes;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "schools")
+public class School {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @NotNull
+    @NotBlank
+    @Column(name = "name")
+    private String name;
+
+    @NotNull
+    @NotBlank
+    @Column(name = "department")
+    private String department;
+
+    @NotNull
+    @NotBlank
+    @Column(name = "enter_date")
+    private LocalDate enterDate;
+
+    @Column(name = "graduate_date")
+    private LocalDate graduateDate;
+
+
+
+}
