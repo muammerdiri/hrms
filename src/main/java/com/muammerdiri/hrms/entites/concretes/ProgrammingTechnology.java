96b5313cc52c21d6ee8dd956ef4e7e58886417fb
Muammer Diri <muammerdiri2@gmail.com>
2022-11-19 19:04:08 +0300

    ProgrammingTechnology created.

--- src/main/java/com/muammerdiri/hrms/entites/concretes/ProgrammingTechnology.java
+++ src/main/java/com/muammerdiri/hrms/entites/concretes/ProgrammingTechnology.java
@@ -1,2 +1,26 @@
-package com.muammerdiri.hrms.entites.concretes;public class ProgrammingTechnology {
+package com.muammerdiri.hrms.entites.concretes;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "programming_technology")
+@Entity
+public class ProgrammingTechnology {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(name = "name")
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "programming_language_id")
+    private ProgrammingLanguage programmingLanguage;
+
+
 }
