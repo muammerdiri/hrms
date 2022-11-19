4dff20cabf9158554a6a26dc1525959b86e8091b
Muammer Diri <muammerdiri2@gmail.com>
2022-11-19 19:27:59 +0300

    JobExperience is releated with JobPosition

---
+++ src/main/java/com/muammerdiri/hrms/entites/concretes/JobExperience.java
@@ -0,0 +1,41 @@
+package com.muammerdiri.hrms.entites.concretes;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+import java.util.Set;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "job_experience")
+@Entity
+public class JobExperience {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @NotNull
+    @NotBlank
+    @Column(name = "company_name")
+    private String companyName;
+
+    @NotNull
+    @NotBlank
+    @Column(name = "enter_date")
+    private LocalDate enterDate;
+
+    @Column(name = "finish_date")
+    private LocalDate finishDate;
+
+
+    @ManyToOne
+    @JoinColumn(name = "job_position_id")
+    private JobPosition jobPosition;
+
+}
